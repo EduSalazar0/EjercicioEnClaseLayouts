8b08cf3d4531e569b9d0337b7e19857beb910ddd
EduSalazar0 <edutebansalazar95@gmail.com>
2024-11-18 07:43:57 -0500

    Agregar archivos de proyecto.

---
+++ EjercicioEnClaseLayouts/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EjercicioEnClaseLayouts
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
