dda93df99e0baaa94832f5cdbda7c1843e44ffe1
EduSalazar0 <edutebansalazar95@gmail.com>
2024-11-18 20:12:07 -0500

    Configuracion de Pagina Main con su boton e imagen.

--- EjercicioEnClaseLayouts/MainPage.xaml.cs
+++ EjercicioEnClaseLayouts/MainPage.xaml.cs
@@ -9,33 +9,12 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        
         private void Boton1_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new PaginaGrid());
         }
-        private void Boton2_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new PaginaAbsolute ());
-        }
-        private void Boton3_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new PaginaFlex());
-        }
-        private void Boton4_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new PaginaStack());
-        }
+        
     }
     
 
